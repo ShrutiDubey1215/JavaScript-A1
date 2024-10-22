98fa89a0cccc455c7980ab9ee4338a5699863dc8
Shruti Dubey <shrutid2626@gmail.com>
2024-10-23 01:22:53 +0530

    Add files via upload

---
+++ indexassignment.js
@@ -0,0 +1,166 @@
+/*
+QUESTION 1: ADD TWO NUMBER
+function AddTwoNumber(a, b){
+    return a+b;
+}
+let a = parseInt(prompt("a="));
+let b = parseInt(prompt("b="));
+if (1<=a && b<=5001)
+{
+var sum = AddTwoNumber(a, b);
+console.log(`${sum}`);
+}
+else
+{
+    console.log("Please enter correct value of a and b")
+}*/
+
+
+
+
+/*
+QUESTION 2: FIND IF THE CONDITION ARE OBEYED OR NOT ?
+function is_valid(a,b){
+    return a,b;
+}
+let a = parseInt(prompt("a="));
+let b = parseInt(prompt("b="));
+if (1<=a && b<=5001)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false")
+}*/
+
+
+
+
+
+/*
+QUESTION 3: CHECK THE CONDITION
+
+function check(a,b){
+    return a,b;
+}
+let a = parseInt(prompt("a="));
+let b = parseInt(prompt("b="));
+if (a%10==0 && b%10==0)
+{
+    console.log("true");
+}
+else if (a%10==0 || b%10==0)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false")
+}*/
+
+
+
+/*
+QUESTION 4: FIND THE FIRST DIGIT OF A 4 DIGIT NUMBER
+
+function First_Digit(n){
+    return String(n)[0];
+}
+let n = parseInt(prompt("n="));
+if (1000<=n && n<=9999)
+{
+    const getfirstdigit = First_Digit(n);
+    console.log("first digit of number is", getfirstdigit);
+}
+else
+{
+    console.log("enter the correct value")
+
+}*/
+
+
+
+/*QUESTION 5: FIND THE LAST DIGIT OF A 4 DIGIT NUMBER
+
+function Last_Digit(n){
+    return String(n)[3];
+}
+let n = parseInt(prompt("n="));
+if (1000<=n && n<=9999)
+{
+    const getLastdigit = Last_Digit(n);
+    console.log("last  digit of number is", getLastdigit);
+}
+else
+{
+    console.log("enter the correct value")
+
+}*/
+
+
+
+
+
+/*
+QUESTION 6: FIND THE REMAINDER
+
+function Find_the_remainder(a,b){
+    return b%a;
+}
+let a = parseInt(prompt("a="));
+let b = parseInt(prompt("b="));
+
+if (a>=1 && b<=10001)
+
+{
+    console.log(`${Find_the_remainder(a,b)}`)
+}
+else {
+    console.log('enter correct value of a and b')
+}*/
+
+
+/*
+Question 7: Multiply two number
+function Multiply_two_number(a,b){
+    return a*b;
+}
+let a = parseInt(prompt("a="));
+let b = parseInt(prompt("b="));
+if (a>=1 && b<=301)
+{
+    var c = Multiply_two_number(a,b);
+    console.log(`${c}`);
+}
+else {
+    console.log('enter the correct value')
+}*/
+
+
+
+/*Question 8: Marks Calculator
+
+function Sum(a,b,c){
+    return a+b+c;
+
+}
+function Average(avg){
+    return avg / 3;
+}
+let a = parseInt(prompt("a="));
+let b = parseInt(prompt("b="));
+let c = parseInt(prompt("c="));
+if (0<=a ,b && c<=100)
+{
+
+    var d = Sum(a,b,c);
+    var e = Average(d);
+    console.log(`${d}`);
+    console.log(`${e}`);
+}
+else {
+    console.log('enter the correct value')
+}*/
+
+
